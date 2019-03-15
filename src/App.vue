ad476cb1403639c9e7ceacaecb42e3ae0bfb3878
Miguel Caberta <caber_t@hotmail.com>
2019-03-15 13:43:41 +0100

    First Commit -landing

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+#app
+  p(v-show="showValue") {{ value }}
+  p(v-if="showValue") {{ value }}
+  p(v-else-if="false") {{ 'algo mas' }}
+  p(v-else) {{ 'Lo ultimo' }}
+</template>
+
+<script>
+export default {
+  name: 'app',
+
+  data () {
+    return {
+      showValue: false,
+      value: 'Algo'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./scss/main.scss"
+</style>
